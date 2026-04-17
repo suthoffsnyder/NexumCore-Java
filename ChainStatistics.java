473785348bca0180106b117979ab96371ec3856f
suthoffsnyder <suthoffsnyder@cdzhongguan.com>
2026-04-17 21:58:28 +0800

    Create ChainStatistics.java

---
+++ ChainStatistics.java
@@ -0,0 +1,12 @@
+package com.nexum.core.statistics;
+
+public class ChainStatistics {
+
+    public long getTotalTransactions() {
+        return 1280000L;
+    }
+
+    public long getActiveAccounts() {
+        return 42600L;
+    }
+}
