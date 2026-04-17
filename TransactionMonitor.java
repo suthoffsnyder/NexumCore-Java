88d109d30568e84b97551fad55b94fe733350477
suthoffsnyder <suthoffsnyder@cdzhongguan.com>
2026-04-17 21:54:48 +0800

    Create TransactionMonitor.java

---
+++ TransactionMonitor.java
@@ -0,0 +1,12 @@
+package com.nexum.core.monitor;
+
+public class TransactionMonitor {
+
+    public int getPendingTxCount() {
+        return 128;
+    }
+
+    public long getTps() {
+        return 320L;
+    }
+}
