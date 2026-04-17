e5a76be06e5492bf89ce2f8fa4f87bcb3b8f52dc
suthoffsnyder <suthoffsnyder@cdzhongguan.com>
2026-04-17 21:51:38 +0800

    Create P2PNodeServer.java

---
+++ P2PNodeServer.java
@@ -0,0 +1,32 @@
+package com.nexum.core.p2p;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class P2PNodeServer {
+    private final int port;
+
+    public P2PNodeServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws InterruptedException {
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(boss, worker)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+            ChannelFuture future = bootstrap.bind(port).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            worker.shutdownGracefully();
+            boss.shutdownGracefully();
+        }
+    }
+}
