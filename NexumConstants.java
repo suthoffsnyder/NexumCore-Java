a9a66577481dd325ce5eb305660e5597ee776273
suthoffsnyder <suthoffsnyder@cdzhongguan.com>
2026-04-17 22:00:21 +0800

    Create NexumConstants.java

---
+++ NexumConstants.java
@@ -0,0 +1,8 @@
+package com.nexum.core.constant;
+
+public class NexumConstants {
+    public static final int BLOCK_INTERVAL = 5000;
+    public static final int MAX_TRANSACTION_PER_BLOCK = 1200;
+    public static final String CHAIN_ID = "nexum-mainnet-01";
+    public static final long GENESIS_TIMESTAMP = 1735689600L;
+}
