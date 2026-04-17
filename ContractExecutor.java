ba0003d1c3596fd878cf6f60d2dd478ffa191c04
suthoffsnyder <suthoffsnyder@cdzhongguan.com>
2026-04-17 21:57:06 +0800

    Create ContractExecutor.java

---
+++ ContractExecutor.java
@@ -0,0 +1,8 @@
+package com.nexum.core.contract;
+
+public class ContractExecutor {
+
+    public String run(String contract, String input) {
+        return "Executed => " + contract + " | Input => " + input;
+    }
+}
